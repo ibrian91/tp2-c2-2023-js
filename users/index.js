676fb6e0a3b561ec9f57fc4e30d5b44903d483b5
ibrian91 <ibrian.giorgio@gmail.com>
2023-09-16 09:05:00 -0300

    Modificaciones

--- users/index.js
+++ users/index.js
@@ -1,21 +1,14 @@
 const userData = require("./usersData.js");
 
-//console.log(userData.getUsers());
-//console.log(userData.getUser("59b99de3cfa9a34dcd788614"));
+// console.log(userData.getUsers());
 
-console.log(userData.addUser({    
-    name: 'Steve Jobs',
-    email: 'Steve@fakegmail.com',
-    password: 'password1234567'
-  }));
+console.log(userData.getUser("59b99de3cfa9a34dcd788614"));
 
-// console.log(userData.updateUser({    
-//     _id: 'lm9ry7g1qlmtfrun74',
-//     name: 'Steve Jobs',
-//     email: 'Steve@tecnoshare.com',
-//     password: '$2b$12$50w2j63ATGmhVOh2rgdjv.wOd9TV0Jb9Xk/Anms0fxVSvGMf5MwvK'
-//   }));
-
-//console.log(userData.deleteUser('lm9ry7g1qlmtfrun74'));
-
-
+console.log(
+  userData.addUser({
+    //el id no lo pongo
+    name: "Cameron Duran",
+    email: "cameron_duran@fakegmail.com",
+    password: "$2b$12$50w2j63ATGmhVOh2rgdjv.wOd9TV0Jb9Xk/Anms0fxVSvGMf5MwvK",
+  })
+);
